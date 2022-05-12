b828fbe44671aab546766117e8a13b84215135f5
alemandira <100322257+alemandira@users.noreply.github.com>
2022-05-11 21:01:54 -0300

    atualizando js

--- JS/exercicios/ex00/ex011.js
+++ JS/exercicios/ex00/ex011.js
@@ -1,4 +1,4 @@
-var idade = 17
+var idade = 67
 if (idade < 16) {
     console.log(`Você tem ${idade} anos, vc não pode votar.`) 
 } else  if (idade < 18 || idade > 65 ) {
---
+++ JS/exercicios/ex014/ex014.js
@@ -0,0 +1,28 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var msg2 = window.document.getElementById('msg2')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 07
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = '04manha.png'
+        document.body.style.background = 'rgb(245, 168, 29)'
+        msg2.innerText = `Bom Dia`
+    } else if (hora >= 12 && hora <= 18) {
+        // boa tarde
+        img.src = '05tarde.png'
+        document.body.style.background = 'rgb(131, 78, 9)'
+        msg2.innerText = `Boa Tarde`
+    } else {
+        // boa noite
+        img.src = '06noite.png'
+        document.body.style.background = 'rgb(68, 23, 233)'
+        msg2.innerText = `Boa Noite`
+    }
+}
+

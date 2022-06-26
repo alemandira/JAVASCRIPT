8beff6a7238cf0e486f08fabb2996b1ad2cc578b
alemandira <100322257+alemandira@users.noreply.github.com>
2022-06-26 20:48:07 -0300

    atualizar

---
+++ JS/exercicios/passo/Passo.js
@@ -0,0 +1,28 @@
+var c = 1
+do {
+    console.log (`Passo ${c}`)
+    c ++
+} while (c <= 6)
+
+
+
+
+
+/*
+var c = 1
+    while ( c <= 6) {
+        console.log(`Passo ${c}`)
+        c ++
+}
+*/
+
+
+
+/*
+console.log ('Passo 1')
+console.log ('Passo 1')
+console.log ('Passo 1')
+console.log ('Passo 1')
+console.log ('Passo 1')
+console.log ('Passo 1')
+*/
---
+++ JS/reteste/aula015/ambiente.js
@@ -0,0 +1,17 @@
+// Exercicíos para enteder a posição dos vetores.
+//num[3] = 6 <- este comando colocaria o numero 6 na quarta posição
+//num.sort() <- ordena por ordem crescente
+//num.push(1) <- coloca o numero 1 na ultima posição
+//num.length <- para verificar quantas posições tem 
+
+
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem: ${num.length} posições.`)
+console.log(`Nosso vetor é : ${num[4]}`) 
+
+//apertar tecla F8 para ver o resultado o exercicio funcionando.
+// 21:15
+

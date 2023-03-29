cb488e990a891767b633c911a0d80a904b3b08c8
alemandira <100322257+alemandira@users.noreply.github.com>
2023-03-28 21:31:21 -0300

    update

--- JS/exercicios/ex00/ex008.js
+++
@@ -1,7 +0,0 @@
-var vel = 54.1
-console.log(`A velocidade do seu carro  é ${vel} KM/H`)
-if (vel > 60 ) {//condição simples
-    console.log(`Você ultrapassou a velocidade limite de sugurança!  Multado`)
-} 
-console.log(`Dirija sempre usando o cinto de segurança`)
-
--- JS/exercicios/ex00/ex011.js
+++
@@ -1,8 +0,0 @@
-var idade = 67
-if (idade < 16) {
-    console.log(`Você tem ${idade} anos, vc não pode votar.`) 
-} else  if (idade < 18 || idade > 65 ) {
-    console.log(`Você tem ${idade} anos, o voto é opcional.`)
-} else {
-    console.log(`Você tem ${idade} anos, o voto é obrigatório.`)
-}

6c560977c0cc0fa386267d207b552821fe33c0c2
alemandira <alemandira@yahoo.com.br>
2023-07-13 18:07:00 -0300

    update

--- exercicio/ex016/script.js
+++ exercicio/ex016/script.js
@@ -3,16 +3,22 @@ function contar() {
     var txtf = document.getElementById('txtf')
     var txtp = document.getElementById('txtp')
 
-    
+
     var ini = Number(txti.value)
     var fim = Number(txtf.value)
     var passo = Number(txtp.value)
-    
+
 
 
     if (ini <= 0 || fim <= 0 || passo <= 0) {
         window.alert('[ERRO] Dados incorretos tente novamente...')
-    } else  {
-        alert('tudo ok.')
+    } else {
+
+        res.innerHTML = `Contando: `
+        for (let c = ini; c <= fim; c += passo) {
+            res.innerHTML += `${c}  \u{1F449}, `
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
+
 }
